4b27e75d010d77d17bab491827a05b044f79de71
ViSiOnOp19cr <chandancr98195@gmail.com>
2025-10-24 23:11:39 +0530

    updated index file

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,7 +3,10 @@ import {PORT} from './config.ts';
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { setupSocketHandlers } from './sockets/index.ts';
-import {router} from './routes/index.ts';
+import {router} from './routes/auth.routes.ts';
+import { roomRouter } from './routes/room.routes.ts';
+import { pollRouter } from './routes/poll.routes.ts';
+import { teacherRouter } from './routes/teacher.routes.ts';
 
 
 const app = express();
@@ -11,6 +14,9 @@ const httpServer = createServer(app);
 
 app.use(express.json());
 app.use('/api/v1', router);
+app.use('/api/v1/rooms', roomRouter);
+app.use('/api/v1/polls', pollRouter);
+app.use('/api/v1/teacher', teacherRouter);
 
 export const io = new Server(httpServer, {
     cors:{
